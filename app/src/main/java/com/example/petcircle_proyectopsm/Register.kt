ecd80ce21c842ae6967f91eb42e5a7e964de0f0a
Isis Flores <isisthechick@gmail.com>
2024-11-14 13:04:19 -0600

    Validaciones de registro de usuario

--- app/src/main/java/com/example/petcircle_proyectopsm/Register.kt
+++ app/src/main/java/com/example/petcircle_proyectopsm/Register.kt
@@ -1,23 +1,221 @@
 package com.example.petcircle_proyectopsm
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Patterns
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.petcircle_proyectopsm.databinding.ActivityRegisterBinding
 
 class Register : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var imageViewProfile: ImageView
+    private val SELECT_IMAGE_REQUEST_CODE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        emailFocusListener()
+        passwordFocusListener()
+        phoneFocusListener()
+        nameFocusListener()
+
+        binding.submitButton.setOnClickListener { submitForm() }
+
+        imageViewProfile = binding.imageViewProfile
+        val buttonSelectImage = binding.buttonSelectImage
+        buttonSelectImage.setOnClickListener {
+            openGallery()
         }
 
-
-
     }
+
+    private fun submitForm()
+    {
+        binding.emailContainer.helperText = validEmail()
+        binding.passwordContainer.helperText = validPassword()
+        binding.phoneContainer.helperText = validPhone()
+        binding.NameContainer.helperText = validName()
+
+        val validEmail = binding.emailContainer.helperText == null
+        val validPassword = binding.passwordContainer.helperText == null
+        val validPhone = binding.phoneContainer.helperText == null
+        val validName = binding.NameContainer.helperText == null
+
+        if (validEmail && validPassword && validPhone && validName)
+            resetForm()
+        else
+            invalidForm()
+    }
+
+    private fun invalidForm()
+    {
+        var message = ""
+        if(binding.emailContainer.helperText != null)
+            message += "\n\nEmail: " + binding.emailContainer.helperText
+        if(binding.passwordContainer.helperText != null)
+            message += "\n\nPassword: " + binding.passwordContainer.helperText
+        if(binding.phoneContainer.helperText != null)
+            message += "\n\nPhone: " + binding.phoneContainer.helperText
+        if(binding.NameContainer.helperText != null)
+            message += "\n\nName: " + binding.NameContainer.helperText
+
+        AlertDialog.Builder(this)
+            .setTitle("Invalid Form")
+            .setMessage(message)
+            .setPositiveButton("Okay"){ _,_ ->
+                // do nothing
+            }
+            .show()
+    }
+
+    private fun resetForm()
+    {
+        var message = "Email: " + binding.emailEditText.text
+        message += "\nPassword: " + binding.passwordEditText.text
+        message += "\nPhone: " + binding.phoneEditText.text
+        message += "\nName: " + binding.NameEditText.text
+        AlertDialog.Builder(this)
+            .setTitle("Form submitted")
+            .setMessage(message)
+            .setPositiveButton("Okay"){ _,_ ->
+                binding.emailEditText.text = null
+                binding.passwordEditText.text = null
+                binding.phoneEditText.text = null
+                binding.NameEditText.text = null
+
+                binding.emailContainer.helperText = getString(R.string.required)
+                binding.passwordContainer.helperText = getString(R.string.required)
+                binding.phoneContainer.helperText = getString(R.string.required)
+                binding.NameContainer.helperText = getString(R.string.required)
+            }
+            .show()
+    }
+
+    private fun emailFocusListener()
+    {
+        binding.emailEditText.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+            {
+                binding.emailContainer.helperText = validEmail()
+            }
+        }
+    }
+
+    private fun validEmail(): String?
+    {
+        val emailText = binding.emailEditText.text.toString()
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailText).matches())
+        {
+            return "Invalid Email Address"
+        }
+        return null
+    }
+
+    private fun passwordFocusListener()
+    {
+        binding.passwordEditText.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+            {
+                binding.passwordContainer.helperText = validPassword()
+            }
+        }
+    }
+
+    private fun validPassword(): String?
+    {
+        val passwordText = binding.passwordEditText.text.toString()
+        if(passwordText.length < 8)
+        {
+            return "Minimum 8 Character Password"
+        }
+        if(!passwordText.matches(".*[A-Z].*".toRegex()))
+        {
+            return "Must Contain 1 Upper-case Character"
+        }
+        if(!passwordText.matches(".*[a-z].*".toRegex()))
+        {
+            return "Must Contain 1 Lower-case Character"
+        }
+        if(!passwordText.matches(".*[@#\$%^&+=.].*".toRegex()))
+        {
+            return "Must Contain 1 Special Character (@#\$%^&+=)"
+        }
+
+        return null
+    }
+
+    private fun phoneFocusListener()
+    {
+        binding.phoneEditText.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+            {
+                binding.phoneContainer.helperText = validPhone()
+            }
+        }
+    }
+
+    private fun validPhone(): String?
+    {
+        val phoneText = binding.phoneEditText.text.toString()
+        if(!phoneText.matches(".*[0-9].*".toRegex()))
+        {
+            return "Must be all Digits"
+        }
+        if(phoneText.length != 10)
+        {
+            return "Must be 10 Digits"
+        }
+        return null
+    }
+
+    private fun nameFocusListener()
+    {
+        binding.NameEditText.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+            {
+                binding.NameContainer.helperText = validName()
+            }
+        }
+    }
+
+    private fun validName(): String?
+    {
+        val nameText = binding.NameEditText.text.toString()
+
+        if(nameText.length < 3)
+        {
+            return "Minimum 3 Characters Name"
+        }
+        if(nameText.length > 30)
+        {
+            return "Maximun 30 Characters Name"
+        }
+        return null
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, SELECT_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SELECT_IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val imageUri: Uri? = data?.data
+            imageUri?.let {
+                imageViewProfile.setImageURI(it)  // Muestra la imagen seleccionada
+            }
+        }
+    }
+
 }
